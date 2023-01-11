afbb9b2d9ba434a133a1c8424cb8a001f45e453e
htchr <john.w.hatcher@gmail.com>
2023-01-10 16:35:23 -0800

    scraper looks like it works, it doesn't like svgs

---
+++ IOscraper.py
@@ -0,0 +1,24 @@
+import requests
+from bs4 import BeautifulSoup
+
+sites = ["https://gixlabs.github.io"]
+sites_dict = {"https://gixlabs.github.io": None}
+
+for s in sites:
+    r = requests.get(s)
+    code = int(r.status_code)
+    # print(code, " - ", s)
+    sites_dict[s] = code
+    if code < 400:
+        data = r.text
+        soup = BeautifulSoup(data, features="html.parser")
+        links = [link.get("href") for link in soup.find_all("a", href=True) if "https" not in link.get("href")]
+        for l in links:
+            link_site = sites[0] + l
+            if link_site not in sites_dict:
+                sites_dict[link_site] = None
+                sites.append(link_site)
+
+for s in sites_dict.keys():
+    if sites_dict[s] >= 400:
+        print(sites_dict[s], " - ", s)
